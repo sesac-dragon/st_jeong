c5f8681f7fc01df41e8707ba50f7cd4fabf73eec
tnlwy25 <tnlwy25@gmail.com>
2025-07-21 16:05:45 +0900

    250721_1

--- app.py
+++ app.py
@@ -1,39 +1,13 @@
 import streamlit as st
+import os
 
-@st.cache_data
-def load_data():
-    return
+st.title('배포 테스트')
+st.write('배포 테스트용 앱입니다.')
 
-load_data()
+api_key = os.environ.get('API_SECRET_KEY')
 
-st.title('my first app')
-st.write('hello')
-st.write('안녕')
+st.write(f'api key = {api_key}')
 
-with st.sidebar:
-    st.title('메뉴')
-
-col = st.columns(3)
-with col[0]:
-    st.metric('주가', '3.77', 0.2)
-with col[1]:
-    st.metric('거래량', '5.4m', -0.7)
-
-with col[2]:
-    st.metric('abc', 'def', 123)
-
-with st.expander('상세정보'):
-    st.table([
-        'a', 'b', 'c'
-    ])
-
-v = st.button('클릭')
-print(v)
-
-if v:
-    st.write('버튼 클릭됨.')
-
-st.slider('볼륨')
-
-name = st.text_input('이름')
-st.write(name)
+# if not os.environ.get('DB_HOST'):
+#     st.write('DB 접속정보가 없습니다.')
+# else:
